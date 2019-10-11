85daa778e1c49eccc5aee2887f787206b8308db4
vkandoba <vkandoba@skbkontur.ru>
2019-10-12 02:07:45 +0500

    работа с изображениями - черно-белое и контрастность

---
+++ Borderer/ImageTool.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Borderer
+{
+    public static class ImageTool
+    {
+        public static Bitmap ToGrayStyle(this Bitmap image)
+        {
+            var bmp = new Bitmap(image.Width, image.Height);
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    var p = image.GetPixel(x, y);
+                    var rgb = (int)Math.Round(0.299 * p.R + 0.587 * p.G + .114 * p.B);
+                    bmp.SetPixel(x, y, Color.FromArgb(rgb, rgb, rgb));
+                }
+            }
+
+            return bmp;
+        }
+
+        public static Bitmap AdjustContrast(this Bitmap image, float Value)
+        {
+            var bmp = new Bitmap(image.Width, image.Height);
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    var p = image.GetPixel(x, y);
+                    float Red = p.R / 255.0f;
+                    float Green = p.G / 255.0f;
+                    float Blue = p.B / 255.0f;
+                    Red = (((Red - 0.5f) * Value) + 0.5f) * 255.0f;
+                    Green = (((Green - 0.5f) * Value) + 0.5f) * 255.0f;
+                    Blue = (((Blue - 0.5f) * Value) + 0.5f) * 255.0f;
+                    bmp.SetPixel(x, y, Color.FromArgb(Clamp((int)Red), Clamp((int)Green), Clamp((int)Blue)));
+                }
+            }
+
+            return bmp;
+        }
+
+        private static int Clamp(int n)
+        {
+            if (n < 0) return 0;
+            if (n > 255) return 255;
+            return n;
+        }
+
+    }
+}
---
+++ BordererTests/ImageToolTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Linq;
+using Borderer;
+using NUnit.Framework;
+
+namespace BordererTests
+{
+    public class ImageToolTests : TestBase
+    {
+        [Test]
+        public void TestToGrayStyle()
+        {
+            foreach (var imagefile in Directory.GetFiles(imageset).Take(10))
+            {
+                var slices = Slice.GenerateBaseSlices(64);
+                var square = Square.MakeSquare(slices);
+
+                var imageName = Path.GetFileName(imagefile);
+                var image = new Bitmap(imagefile);
+                var sourcefile = Path.Combine(imagesource, imageName);
+                var source = new Bitmap(sourcefile);
+
+                var bitmap1 = square.Draw(image);
+                var bitmap2 = square.Draw(source);
+                var bitmap3 = image.ToGrayStyle();
+                var bitmap4 = source.ToGrayStyle();
+                bitmap1.Save($"C:\\huaway\\tests\\image-{imageName}");
+                bitmap2.Save($"C:\\huaway\\tests\\source-{imageName}");
+                bitmap3.Save($"C:\\huaway\\tests\\image-gray-{imageName}");
+                bitmap4.Save($"C:\\huaway\\tests\\source-gray-{imageName}");
+            }
+        }
+
+        [Test]
+        public void TestTAdjustContrast()
+        {
+            foreach (var imagefile in Directory.GetFiles(imageset).Take(10))
+            {
+                var slices = Slice.GenerateBaseSlices(64);
+                var square = Square.MakeSquare(slices);
+
+                var imageName = Path.GetFileName(imagefile);
+                var name = Path.GetFileNameWithoutExtension(imagefile);
+                var image = new Bitmap(imagefile);
+                var sourcefile = Path.Combine(imagesource, imageName);
+                var source = new Bitmap(sourcefile);
+
+                var bitmap1 = square.Draw(image);
+                var bitmap2 = square.Draw(source);
+                var bitmap3 = image.AdjustContrast(2);
+                var bitmap4 = source.AdjustContrast(2);
+                bitmap1.Save($"C:\\huaway\\tests\\{imageName}-split.png");
+                bitmap2.Save($"C:\\huaway\\tests\\{imageName}.png");
+                bitmap3.Save($"C:\\huaway\\tests\\{imageName}-split-contrast.png");
+                bitmap4.Save($"C:\\huaway\\tests\\{imageName}-contrast.png");
+            }
+        }
+    }
+}
